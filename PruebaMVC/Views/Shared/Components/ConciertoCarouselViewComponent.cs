c4ac0889400a4ab1bdb54f09380304ebf49583a2
Asambler2 <chema508@hotmail.com>
2024-06-11 14:37:34 +0200

    Enpezado a hacer el componente

---
+++ PruebaMVC/Views/Shared/Components/ConciertoCarouselViewComponent.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PruebaMVC.Models;
+using PruebaMVC.Services.Repositorio;
+
+namespace PruebaMVC.Views.Shared.Componets
+{
+    public class ConciertoCarouselViewComponent(IGenericRepositorio<Concierto> _contextConcierto) : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View(await _contextConcierto.DameTodos());
+        }
+    }
+}
